b4a59b5755c54c4828958fb9eec8440bf9171d9e
BuildTools <frajerzycki@protonmail.com>
2019-09-21 17:25:32 +0200

    generate_password generates character without separate function.

--- secure_generator.py
+++ secure_generator.py
@@ -1,20 +1,14 @@
 from secrets import randbelow
 from itertools import chain
-import string
-def generate_password(length, should_have_whitespaces=False, should_have_punctuation=False, should_have_digits=True):
-    password = ""
-    for _ in range(length):
-        password += generate_char(should_have_whitespaces, should_have_punctuation, should_have_digits)
-    return password
-def generate_char(should_have_whitespaces, should_have_punctuation, should_have_digits):
-    characters = string.ascii_letters
-    if should_have_whitespaces:
-        characters += string.whitespace
-    if should_have_punctuation:
-        characters += string.punctuation
-    if should_have_digits:
-        characters += string.digits
-    return characters[randbelow(len(characters))]
-for x in range(100):
-    print(generate_password(x, False, True, True))
+from string import ascii_letters, whitespace, punctuation, digits 
 
+# Generates random passwords with desired length.
+def generate_password(length, should_have_whitespaces, should_have_punctuation, should_have_digits):
+    characters = ascii_letters
+    if should_have_whitespaces:
+        characters += whitespace
+    if should_have_punctuation:
+        characters += punctuation
+    if should_have_digits:
+        characters += digits
+    return ''.join(characters[randbelow(len(characters))] for _ in range(length))
