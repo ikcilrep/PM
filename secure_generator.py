0b1da4bd1b23ad6603b821de5c43ae8ad36af0ec
Frajerzycki <32599796+Frajerzycki@users.noreply.github.com>
2019-09-21 17:25:24 +0200

    Function comment changed.

--- secure_generator.py
+++ secure_generator.py
@@ -2,7 +2,7 @@ from secrets import randbelow
 from itertools import chain
 from string import ascii_letters, whitespace, punctuation, digits 
 
-# Generates random passwords with desired length.
+# Generates random password with desired length.
 def generate_password(length, should_have_whitespaces, should_have_punctuation, should_have_digits):
     characters = ascii_letters
     if should_have_whitespaces:
