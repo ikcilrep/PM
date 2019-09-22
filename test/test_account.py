c473823c6601c0158d949a296c223f4c399d742b
BuildTools <frajerzycki@protonmail.com>
2019-09-22 18:13:23 +0200

    Account class methods test written.

---
+++ test/test_account.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+from app.security.AES import derive_key 
+from app.account import Account, decrypt_account
+
+class TestAccount(unittest.TestCase):
+    def test(self):
+        account = Account("service", "username","email", "password") 
+        key = derive_key("user_password")
+        self.assertEquals(account, decrypt_account(account.encrypt(key), key)) 
