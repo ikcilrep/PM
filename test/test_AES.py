2d113bf1b3af0c34796c6d11304f5c1202629cbb
BuildTools <frajerzycki@protonmail.com>
2019-09-22 18:12:37 +0200

    Imports changed.

--- test/test_AES.py
+++ test/test_AES.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
-from security import AES, secure_generator
+from app.security import AES, secure_generator
 
 
-class Test(unittest.TestCase):
+class TestAES(unittest.TestCase):
     def test(self):
         for x in range(512):
-            key = AES.derive_key(secure_generator.generate_password(x, False, True, True))
-            message = secure_generator.generate_password(x, False, True, True) # Random string to be encrypted.
+            key = AES.derive_key(secure_generator.generate_password(x, True, True, True))
+            message = secure_generator.generate_password(x, True, True, True) # Random string to be encrypted.
             self.assertEqual(AES.decrypt(AES.encrypt(
                 message, key), key), message)
