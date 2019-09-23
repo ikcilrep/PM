9bd456221bc16bfc3afa077c015cc4c477b43275
BuildTools <frajerzycki@protonmail.com>
2019-09-23 15:44:16 +0200

    add_account function is moved from assign_accounts to user, accounts variable are initialized in init.

--- app/user.py
+++ app/user.py
@@ -2,30 +2,45 @@ from account import Account, decrypt_account
 from security.AES import derive_key
 from security.password_storing import hash_digest
 
+# Returns decrypted user.
+
+
+def decrypt_user(encrypted_user, password):
+    user = User(
+        encrypted_user['username'],
+        derive_key(password, encrypted_user['AES_key_salt']),
+        password,
+        encrypted_user['hashed_password']
+    )
+
+    user.assign_accounts(encrypted_user['encrypted_accounts'])
+    return user
+
+# Class representing user of this application.
+
 
 class User:
     def __init__(self, username, AES_key, password, hashed_password):
+        self.accounts = {}
         self.username = username
         self.AES_key = AES_key
         self.__hashed_password = hashed_password
         self.__password = password
         self.encrypt = lambda: {
-                'username': username,
-                'encrypted_accounts': [self.accounts[key].encrypt(self.AES_key) for key in self.accounts], 
-                'hashed_password': self.__hashed_password, 
-                'AES_key_salt': self.AES_key['salt']
-                }
+            'username': username,
+            'encrypted_accounts': [self.accounts[key].encrypt(self.AES_key) for key in self.accounts],
+            'hashed_password': self.__hashed_password,
+            'AES_key_salt': self.AES_key['salt']
+        }
+    def add_account(self, account):
+        self.accounts[account.service_name] = account
 
 
+    # Decrypts and assigns accounts to this user.
 
-    def assign_accounts(self, encrypted_accounts, AES_key):
-        def add_account(self, account):
-            self.accounts[account.service_name] = account
-
-        self.accounts = {}
+    def assign_accounts(self, encrypted_accounts):
         for encrypted_account in encrypted_accounts:
-            self.add_account(decrypt_account(encrypted_accounts, AES_key))
-    
+            self.add_account(decrypt_account(encrypted_accounts, self.AES_key))
 
     @property
     def password(self):
