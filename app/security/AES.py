bf14be641d623ca40ee57b0d7fe0bcb495d8ef5f
frajerzycki <frajerzycki@protonmail.com>
2019-10-04 23:14:21 +0200

    Unnecessary argument removed.

--- app/security/AES.py
+++ app/security/AES.py
@@ -12,7 +12,7 @@ pkcs7 = padding.PKCS7(128)
 # Returns pbkdf2 derivators: one for key, one for IV.
 def derive_key(password, salt=to_b64_str(urandom(16))):
     return {
-            'key': to_b64_str(PBKDF2HMAC(algorithm=SHA256, length=32, salt=from_b64_str(salt), iterations=100000, backend=backend).derive(password.encode('utf-8'))),
+            'key': to_b64_str(PBKDF2HMAC(algorithm=SHA256, length=32, salt=from_b64_str(salt), iterations=100000, backend=backend).derive(password.encode())),
             'salt': salt
             }
 
@@ -24,7 +24,7 @@ def encrypt(data, key):
     IV = urandom(16)
     encryptor = Cipher(algorithms.AES(from_b64_str(key['key'])), modes.CBC(IV),
                        backend=backend).encryptor()
-    return {'ciphertext': to_b64_str(encryptor.update(padder.update(data.encode('utf-8')) + padder.finalize()) + encryptor.finalize()), 'IV': to_b64_str(IV)}
+    return {'ciphertext': to_b64_str(encryptor.update(padder.update(data.encode()) + padder.finalize()) + encryptor.finalize()), 'IV': to_b64_str(IV)}
 
 # Returns returns decrypted data.
 
@@ -33,4 +33,4 @@ def decrypt(encrypted_data, key):
     unpadder = pkcs7.unpadder()
     decryptor = Cipher(algorithms.AES(from_b64_str(key['key'])), modes.CBC(from_b64_str(
         encrypted_data['IV'])), backend=backend).decryptor()
-    return (unpadder.update(decryptor.update(from_b64_str(encrypted_data['ciphertext'])) + decryptor.finalize()) + unpadder.finalize()).decode('utf-8')
+    return (unpadder.update(decryptor.update(from_b64_str(encrypted_data['ciphertext'])) + decryptor.finalize()) + unpadder.finalize()).decode()
--- app/security/password_storing.py
+++ app/security/password_storing.py
@@ -8,9 +8,9 @@ def get_scrypt_derivator(salt): return Scrypt(
 
 # Returns True if password match with stored hash.
 def verify_password(password, hash_digest): return to_b64_str(get_scrypt_derivator(
-    from_b64_str(hash_digest['salt'])).derive(password.encode('utf-8'))) == hash_digest['hash']
+    from_b64_str(hash_digest['salt'])).derive(password.encode())) == hash_digest['hash']
 
 # Returns dictionary with salt ('salt') and result of scrypt derivation for password ('hash'). 
 def hash_digest(password):
     salt = urandom(16)
-    return {'salt': to_b64_str(salt), 'hash': to_b64_str(get_scrypt_derivator(salt).derive(password.encode('utf-8')))}
+    return {'salt': to_b64_str(salt), 'hash': to_b64_str(get_scrypt_derivator(salt).derive(password.encode()))}
